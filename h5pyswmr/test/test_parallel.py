d110816a7ab4330b3481aa86a82c650a146740b3
Remo Goetschi <remo.goetschi@meteotest.ch>
2015-03-05 17:46:41 +0100

    improved unit test

--- h5pyswmr/test/test_parallel.py
+++ h5pyswmr/test/test_parallel.py
@@ -5,6 +5,8 @@ import sys
 import os
 import multiprocessing
 import tempfile
+import time
+import random
 
 import numpy as np
 
@@ -21,7 +23,7 @@ from h5pyswmr import File
 class TestHDF5(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.shape = (8000, 1500)
 
     def test_parallel(self):
         """
@@ -30,33 +32,39 @@ class TestHDF5(unittest.TestCase):
 
         tmpdir = tempfile.gettempdir()
 
-        NO_WORKERS = 20
+        NO_WORKERS = 30
         filename = os.path.join(tmpdir, 'parallel_test.h5')
         f = File(filename, 'w')
         # create some datasets (to test reading)
         for i in range(NO_WORKERS):
-            f.create_dataset(name='/testgrp/dataset{}'.format(i), data=np.random.random((2000, 1400)))
+            f.create_dataset(name='/testgrp/dataset{}'.format(i), data=np.random.random(self.shape).astype(np.float32))
 
         # raw_input('Press <enter> to start {} reader/writer processes '.format(NO_WORKERS))
 
         def worker_read(i, hdf5file):
             # do some reading
+            time.sleep(random.random())
+            print("worker {0} is reading...".format(i))
             data = hdf5file['/testgrp/dataset{}'.format(i)][:]
-            self.assertEqual(data.shape, (2000, 1400))
+            print("worker {0} is done reading.".format(i))
+            self.assertEqual(data.shape, self.shape)
 
         def worker_write(i, hdf5file):
             # do some reading
             # print(hdf5file.keys())
             # do some writing
-            data = np.empty((4, 2000, 1500), dtype=np.int32)
+            time.sleep(random.random())
+            data = np.empty((4, self.shape[0], self.shape[1]), dtype=np.int32)
             data[:] = i*100
-            hdf5file.create_dataset(name='/testgrp/bla{}'.format(i), data=data)
             # modify existing dataset
             dst = hdf5file['/testgrp/dataset{}'.format(i)]
+            print("worker {0} is writing...".format(i))
             dst[0:50, ] = i
+            print("worker {0} done writing.".format(i))
 
         jobs = []
         writers = []
+        print("")
         for i in range(NO_WORKERS):
             if i < 5 or i % 2 == 0:
                 p = multiprocessing.Process(target=worker_read, args=(i, f))
@@ -65,14 +73,14 @@ class TestHDF5(unittest.TestCase):
                 writers.append(i)
             jobs.append(p)
             p.start()
-            p.join()
+            # p.join()
 
         # check if file was written correctly
+        # wait a few seconds to make sure that all workers are done...
+        time.sleep(5)
         for i in writers:
             dst = f['/testgrp/dataset{}'.format(i)]
             self.assertTrue(np.all(dst[0:50, ] == i))
-            dst2 = f['/testgrp/bla{}'.format(i)]
-            self.assertTrue(np.all(dst2[:] == i*100))
 
     def tearDown(self):
         pass
